c8fc87d5e99fd784cfb09c357aa5cc9f83e50b68
Manuel Lamas <moncholamas@gmail.com>
2025-02-04 21:05:35 -0300

    gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [show, setShow] = useState(null)
 
+  
+  const mostrar = () => {
+
+    
+    if ( ! desencrypt.includes('drive') ){
+      // elegiste mal las letras intenta de nuevo
+    } else {
+      // hola
+    }
+    console.log('hola')
+    console.log('https://drive.google.com/file/d/0Bwy8Lg0BGCCQSFZLSzJkS2pnSUU/view?usp=sharing&resourcekey=0-q2d1mX0YPVNUtl1lc_bjLg')
+  }
+
+  useEffect(()=>{
+
+  },[show])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='step_1'>
+        <span>Ingresa el código de tu pulsera.</span>
+          <div className="card">
+            <input type='text' name="c1" className='input' maxLength={1}></input>
+            <input type='text' name="c2" className='input' maxLength={1}></input>
+            <input type='text' name="c3" className='input' maxLength={1}></input>
+            -
+            <input type='text' name="c4" className='input' maxLength={1}></input>
+            <input type='text' name="c5" className='input' maxLength={1}></input>
+            <input type='text' name="c6" className='input' maxLength={1}></input>
+            -
+            <input type='text' name="c7" className='input' maxLength={1}></input>
+            <input type='text' name="c8" className='input' maxLength={1}></input>
+            <input type='text' name="c9" className='input' maxLength={1}></input>
+
+          </div>
+          <div className="card">
+            <button onClick={mostrar}>
+              Abrir Caja
+            </button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className='step_2'>
+        <p className="read-the-docs">
+          Este regalo es anónimo
         </p>
+        <img src='https://drive.google.com/thumbnail?id=0Bwy8Lg0BGCCQSFZLSzJkS2pnSUU/'></img>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
